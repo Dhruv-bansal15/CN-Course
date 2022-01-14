60f4e595adaa09a8cc257ee7b600a31bff40ec2a
Dhruv Bansal <bansaldhruv1973@gmail.com>
2022-01-15 03:30:23 +0530

    BST

---
+++ BST/pair_Sum_in_BST.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~BinaryTreeNode() {
+        if (left) delete left;
+        if (right) delete right;
+    }
+};
+
+using namespace std;
+
+BinaryTreeNode<int> *takeInput() {
+    int rootData;
+
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int> *rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+void printLevelATNewLine(BinaryTreeNode<int> *root) {
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty()) {
+        BinaryTreeNode<int> *first = q.front();
+        q.pop();
+        if (first == NULL) {
+            if (q.empty()) {
+                break;
+            }
+            cout << endl;
+            q.push(NULL);
+            continue;
+        }
+        cout << first->data << " ";
+        if (first->left != NULL) {
+            q.push(first->left);
+        }
+        if (first->right != NULL) {
+            q.push(first->right);
+        }
+    }
+}
+vector<int> arr;
+void fillvector(BinaryTreeNode<int> *root){
+    if(root==NULL){
+        return;
+    }
+    fillvector(root->left);
+    fillvector(root->right);
+    arr.push_back(root->data);
+    return;
+}
+void printNodesSumToS(BinaryTreeNode<int> *root, int s) {
+    // Write your code here
+    if(root==NULL){
+        return;
+    }
+    
+    fillvector(root);
+    sort(arr.begin(),arr.end());
+    int i=0,j=arr.size()-1;
+    while(i<j){
+        if(arr[i]+arr[j] > s){
+            j--;
+        }else if(arr[i]+arr[j]<s){
+            i++;
+        }else{
+            cout<<arr[i]<<" "<<arr[j]<<endl;
+            i++,j--;
+        }
+    }
+    return;
+    
+}
+int main() {
+    BinaryTreeNode<int> *root = takeInput();
+    int s;
+    cin >> s;
+    printNodesSumToS(root, s);
+    delete root;
+}
