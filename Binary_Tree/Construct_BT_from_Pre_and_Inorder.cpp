c092948c3cb5233bbb4d9eccb17a82f64f3237f5
Dhruv Bansal <bansaldhruv1973@gmail.com>
2022-01-15 03:04:13 +0530

    Tree

--- Binary_Tree/Construct_BT_from_Pre_and_Inorder.cpp
+++ Binary_Tree/Construct_BT_from_Pre_and_Inorder.cpp
@@ -19,7 +19,6 @@ using namespace std;
 
 BinaryTreeNode<int> *takeInput() {
     int rootData;
-
     cin >> rootData;
     if (rootData == -1) {
         return NULL;
@@ -31,6 +30,7 @@ BinaryTreeNode<int> *takeInput() {
         BinaryTreeNode<int> *currentNode = q.front();
         q.pop();
         int leftChild, rightChild;
+        
         cin >> leftChild;
         if (leftChild != -1) {
             BinaryTreeNode<int> *leftNode = new BinaryTreeNode<int>(leftChild);
@@ -73,23 +73,38 @@ void printLevelATNewLine(BinaryTreeNode<int> *root) {
         }
     }
 }
-
-void mirrorBinaryTree(BinaryTreeNode<int>* root) {
-    // Write your code here
-    if(root==NULL){
-        return;
+BinaryTreeNode<int>* helper(int *preorder,int *inorder, int inS, int inE, int preS, int preE ){
+    if(inS>inE){
+        return NULL;
     }
-    BinaryTreeNode<int>* left= root->left;
-    BinaryTreeNode<int>* right = root->right;
-    root->left = right;root->right= left;
-    mirrorBinaryTree(right);
-    mirrorBinaryTree(left);
-    return;
-    
+    int rootData = preorder[preS];
+    int rootIndex=-1;
+    for(int i=inS;i<=inE;i++){
+        if(inorder[i]==rootData){
+            rootIndex= i;
+            break;
+        }
+    }
+    int lInS = inS, lInE= rootIndex-1, lPreS = preS+1;
+    int lPreE = lInE - lInS + lPreS;
+    int rPreS = lPreE+1, rPreE = preE, rInS = rootIndex+1, rInE = inE;
+    BinaryTreeNode<int>* root= new BinaryTreeNode<int>(rootData);
+    root->left = helper(preorder, inorder, lInS, lInE, lPreS, lPreE);
+    root->right = helper(preorder, inorder, rInS, rInE, rPreS, rPreE);
+    return root;
+}
+BinaryTreeNode<int>* buildTree(int *preorder, int preLength, int *inorder, int inLength) {
+    // Write your code here
+    return helper(preorder,inorder, 0, preLength-1, 0, preLength-1);
 }
 
 int main() {
-    BinaryTreeNode<int> *root = takeInput();
-    mirrorBinaryTree(root);
+    int size;
+    cin >> size;
+    int *pre = new int[size];
+    int *in = new int[size];
+    for (int i = 0; i < size; i++) cin >> pre[i];
+    for (int i = 0; i < size; i++) cin >> in[i];
+    BinaryTreeNode<int> *root = buildTree(pre, size, in, size);
     printLevelATNewLine(root);
 }
