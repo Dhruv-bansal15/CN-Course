3e4487afb5ca994764e9c43bc7ba86c082312ee5
Dhruv Bansal <bansaldhruv1973@gmail.com>
2022-01-15 01:46:13 +0530

    LL

---
+++ Linked_List_2/Reverse_LL_Iterative.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+class Node {
+   public:
+    int data;
+    Node *next;
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+using namespace std;
+
+Node *takeinput() {
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while (data != -1) {
+        Node *newnode = new Node(data);
+        if (head == NULL) {
+            head = newnode;
+            tail = newnode;
+        } else {
+            tail->next = newnode;
+            tail = newnode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head) {
+    Node *temp = head;
+
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    cout << "\n";
+}
+Node *reverseLinkedList(Node *head) {
+    // Write your code here
+    if(head==NULL){
+        return head;
+    }else if(head->next==NULL){
+        return head;
+    }
+    Node* prev=NULL;
+    Node* curr= head;
+    Node* nex = head->next;
+    while(curr!=NULL){
+        curr->next = prev;
+        prev=curr;
+        curr=nex;
+        if(curr==NULL){
+            break;
+        }
+        nex= nex->next;
+    }
+    return prev;
+}
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        Node *head = takeinput();
+        head = reverseLinkedList(head);
+        print(head);
+    }
+
+    return 0;
+}
