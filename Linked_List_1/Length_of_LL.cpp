96774e01ab6aa41fb675cc9e118daa1e95ce8fcb
Dhruv Bansal <bansaldhruv1973@gmail.com>
2022-01-15 01:22:47 +0530

    LL

---
+++ Linked_List_1/Length_of_LL.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+using namespace std;
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int length(Node *head)
+{
+    //Write your code here
+    if(head==NULL){
+        return 0;
+    }
+    int cnt=1;
+    Node *temp = head;
+    while(temp-> next != NULL){
+        temp = temp-> next;
+        cnt++;
+    }
+    return cnt;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		cout << length(head) << endl;
+	}
+	return 0;
+}
